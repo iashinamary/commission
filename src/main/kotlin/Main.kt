6da7909cf4a97d976e559c1a832a3f3ee23f468d
Mariia Iashina <iashinamary@gmail.com>
2023-11-04 03:20:39 +0300

    fixed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,13 @@
 import kotlin.math.round
 
 fun main() {
-    val amount1 = 30_000
-    val amount2 = 1000
+    val amount = 1000
     val percentOfCommission = 100 * 0.75
 
-    var commission1 = Math.round(amount1 / percentOfCommission)
-    if (commission1 < 35) {
-        commission1 = 35
+    var commission = (amount / percentOfCommission).toInt()
+    if (commission < 35) {
+        commission = 35
     }
 
-    var commission2 = Math.round(amount2 / percentOfCommission)
-    if (commission2 < 35) {
-        commission2 = 35
-    }
-
-    println(commission1)
-    println(commission2)
+    println(commission)
 }
